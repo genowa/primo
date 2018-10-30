430b651447bf7baed1421e061e775fcdd3a74b9e
sabas <sabas88@gmail.com>
2018-10-30 23:18:07 +0100

    css

---
+++ frasi.js
@@ -0,0 +1,25 @@
+var saluti = [
+    "",
+    "Buongiorno",
+    "Ciao",
+    "Gentilissimi"
+];
+    
+var domanda = [
+    "volevo chiedere",
+    "mi interessava sapere",
+    "segnalo che"
+];
+var cosa = [
+    "le scuole sono chiuse",
+    "perche' l'autobus non passa",
+    "i cimiteri sono chiusi",
+    "le pizzerie consegnano"
+];
+
+var finale = [
+    "complimenti per la trasmissione",
+    "grazie",
+    "la saluto",
+    "grazie per il vostro servizio pubblico"
+];
